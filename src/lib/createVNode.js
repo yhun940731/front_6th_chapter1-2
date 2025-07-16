eacf25bd36440c5b28946a1fbf668fbd397e82e0
최용훈 <yonghun@douzone.com>
2025-07-16 16:50:50 +0900

    Feat: createVNode 개발.

--- src/lib/createVNode.js
+++ src/lib/createVNode.js
@@ -1,3 +1,7 @@
 export function createVNode(type, props, ...children) {
-  return {};
+  return {
+    type,
+    props,
+    children: children.flat(Infinity).filter((child) => child !== false && child !== null && child !== undefined),
+  };
 }
