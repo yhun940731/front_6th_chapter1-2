ece769db7a2532fcad708a27dfa6e50e74e97a8b
최용훈 <yonghun@douzone.com>
2025-07-16 16:51:10 +0900

    Feat: normalizeVNdoe 개발.

--- src/lib/normalizeVNode.js
+++ src/lib/normalizeVNode.js
@@ -1,3 +1,15 @@
+const isFalsy = (node) => node === false || node === null || node === undefined;
+
 export function normalizeVNode(vNode) {
+  if (vNode === null || vNode === undefined || typeof vNode === "boolean") return "";
+
+  if (typeof vNode === "string" || typeof vNode === "number") return vNode?.toString();
+
+  if (typeof vNode.type === "function")
+    return normalizeVNode(vNode.type({ ...vNode.props, children: vNode.children.filter((v) => !isFalsy(v)) }));
+
+  if (Array.isArray(vNode.children))
+    return { ...vNode, children: vNode.children.map(normalizeVNode).filter((child) => child !== "") };
+
   return vNode;
 }
