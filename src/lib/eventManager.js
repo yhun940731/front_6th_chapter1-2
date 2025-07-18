5cff1725a4185d3cef5d36409bb3b3fef631ca00
최용훈 <yonghun@douzone.com>
2025-07-18 16:37:09 +0900

    Feat: EventManager 개발.

--- src/lib/eventManager.js
+++ src/lib/eventManager.js
@@ -1,6 +1,6 @@
-export function setupEventListeners(root) {
-  console.log("setupEventListeners called with root:", root);
-}
+// [key: Element]: Map<string, Set<function>>
+const eventManager = new WeakMap();
+const eventTypes = new Set();
 
 /**
  * @param {Element} element
@@ -8,9 +8,59 @@ export function setupEventListeners(root) {
  * @param {function} handler
  */
 export function addEvent(element, eventType, handler) {
-  element.addEventListener(eventType, handler);
+  if (!eventManager.has(element)) {
+    eventManager.set(element, new Map());
+  }
+
+  const eventMap = eventManager.get(element);
+  if (!eventMap.has(eventType)) {
+    eventMap.set(eventType, new Set());
+  }
+
+  const handlerSet = eventMap.get(eventType);
+  handlerSet.add(handler);
+
+  // eventType을 전역 Set에 추가
+  eventTypes.add(eventType);
 }
 
 export function removeEvent(element, eventType, handler) {
-  element.removeEventListener(eventType, handler);
+  const eventMap = eventManager.get(element);
+
+  if (!eventMap) return;
+
+  const handlerSet = eventMap.get(eventType);
+
+  if (!handlerSet) return;
+
+  handlerSet.delete(handler);
+
+  if (handlerSet.size === 0) {
+    eventMap.delete(eventType);
+  }
+
+  if (eventMap.size === 0) eventManager.delete(element);
+}
+
+export function setupEventListeners(root) {
+  // eventTypes를 순회하여 각 이벤트 타입에 대해 위임된 핸들러 설정
+  eventTypes.forEach((eventType) => {
+    root.addEventListener(eventType, (e) => {
+      let currentTarget = e.target;
+
+      // 이벤트 버블링을 통해 상위 요소까지 핸들러 검색
+      while (currentTarget && currentTarget !== root) {
+        const eventMap = eventManager.get(currentTarget);
+
+        if (eventMap && eventMap.has(eventType)) {
+          const handlerSet = eventMap.get(eventType);
+          handlerSet.forEach((handler) => {
+            handler(e);
+          });
+        }
+
+        currentTarget = currentTarget.parentElement;
+      }
+    });
+  });
 }
